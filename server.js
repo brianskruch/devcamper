846640856f0ea6856e4eb37ee856e099dfc11944
Brian Skruch <brian@brian.skruch.com>
2019-11-02 08:35:42 -0500

    Commit after Section 4 Lecture 22

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ connectDB();
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-
-
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
