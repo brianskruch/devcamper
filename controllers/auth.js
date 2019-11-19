475f73145fa83bdbaef5769c53d20b89d460f9d6
Brian Skruch <brian@brian.skruch.com>
2019-11-18 19:53:34 -0600

    Commit after Section 7 Lecture 46

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,5 +7,16 @@ const ErrorResponse = require('../utils/errorResponse');
 // @access  Public
 
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
   res.status(200).json({ success: true });
+
 });
