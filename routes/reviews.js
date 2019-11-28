73c0f5ac0f8fae77bcf5204e03aef8e2afcc7d4f
Brian Skruch <brian@brian.skruch.com>
2019-11-28 06:52:10 -0600

    Commit after Section 9 Lecture 62

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-  getReviews
+  getReviews,
+  getReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -13,11 +14,13 @@ const { protect, authorize } = require('../middleware/auth');
 router
   .route('/')
   .get(
-    advancedResults(Review,{
-    path: 'bootcamp',
-    select: 'name description'
-  }),
-  getReviews
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description'
+    }),
+    getReviews
   );
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
