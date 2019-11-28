04f7251b4649fd39a4cd6e3bcada34470704cba2
Brian Skruch <brian@brian.skruch.com>
2019-11-28 07:58:10 -0600

    Commit after Section 9 Lecture 63

--- models/Review.js
+++ models/Review.js
@@ -14,7 +14,7 @@ const ReviewSchema = new mongoose.Schema({
   rating: {
     type: Number,
     min: 1,
-    max:10,
+    max: 10,
     required: [true, 'Please add rating (1-10)']
   },
   createdAt: {
@@ -33,5 +33,7 @@ const ReviewSchema = new mongoose.Schema({
   }
 });
 
+// Setup so that there can only be one review per user per bootcamp (lecture 63)
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
 module.exports = mongoose.model('Review', ReviewSchema);
