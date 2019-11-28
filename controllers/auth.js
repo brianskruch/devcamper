9ebde2c8c18c1dd775a4387d19184c5c54cfb468
Brian Skruch <brian@brian.skruch.com>
2019-11-28 10:59:03 -0600

    Commit after Section 10 Lecture 66

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,6 +53,24 @@ exports.login = asyncHandler(async (req, res, next) => {
 });
 
 
+// @desc  Log User Out
+// @route POST /api/v1/auth/logout
+// @access  Private
+
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  })
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+
+});
+
+
 // @desc  Get Current Logged in User
 // @route POST /api/v1/auth/me
 // @access  Private
