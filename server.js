1b388570c81dcdfaf41e08a1d579aedba23bad3e
Brian Skruch <brian@brian.skruch.com>
2019-11-28 14:31:20 -0600

    Commit after Section 10 Lecture 67

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -36,6 +37,10 @@ if (process.env.NODE_ENV === 'development') {
 
 // Add middleware for fileupload
 app.use(fileupload());
+
+// Sanatize Data for mongodb insertion hacks
+app.use(mongoSanitize());
+
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
