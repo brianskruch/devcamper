04480af2c136e02e68d91938520a7f9fb2f922b9
Brian Skruch <brian@brian.skruch.com>
2019-11-29 06:14:33 -0600

    Commit after Section 10 Lecture 69

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -41,6 +46,24 @@ app.use(fileupload());
 // Sanatize Data for mongodb insertion hacks
 app.use(mongoSanitize());
 
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 min
+    max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable cross site scripting for public API
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
